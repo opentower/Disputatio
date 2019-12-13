b23edd004d666f9984387f8e6087e405a33c11d1
gleachkr <gleachkr@gmail.com>
2019-12-13 13:05:24 -0600

    Add edges with shift-click

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ var draggable = require('plain_draggable')
 class GraphNode extends HTMLElement {
     constructor(parent,x,y) {
         super();
+        this.edges = [] //initialize array of edges
         let bg = document.createElement("div");
         let input = document.createElement("textarea");
         this.style.position = 'absolute'
@@ -15,6 +16,7 @@ class GraphNode extends HTMLElement {
         input.cols = 5
         input.rows = 1
         input.style.border = 'none'
+        input.graphNode = this
         bg.style.display = 'inline-block'
         bg.style.position = 'absolute'
         bg.style.background = 'white'
@@ -22,22 +24,35 @@ class GraphNode extends HTMLElement {
         bg.style.left = 0
         bg.style.height = '100%'
         bg.style.width = '100%'
+        bg.graphNode = this
         input.style.zIndex = 5;
         this.appendChild(input);
         this.appendChild(bg);
+
+        this.observer = new MutationObserver(_ => this.redrawEdges)
+        this.observer.observe(input, {attributes: true})
         input.addEventListener('focusout', _ => { if (input.value == "") this.detach() })
         this.attach(parent)
-        new PlainDraggable(this, {left:x, top:y, handle:bg});
+        new PlainDraggable(this, {
+            left:x, 
+            top:y, 
+            handle:bg,
+            onMove: _ => this.redrawEdges()
+        });
         input.focus()
     }
     detach() { this.parentNode.removeChild(this); }
 
     attach(parent) { parent.appendChild(this); }
+
+    redrawEdges() { this.edges.forEach(l => l.position()) }
 }
 
 class Graph extends HTMLElement {
     constructor() {
         super();
+
+        this.focalNode //initialize focal node
         this.style.display = 'inline-block'
         this.style.outline = '1px solid'
         this.style.overflow = 'hidden'
@@ -45,6 +60,12 @@ class Graph extends HTMLElement {
         this.addEventListener('click',e => 
             {   if (e.target == this) {
                 this.createNode(e.clientX,e.clientY)
+                } else if (e.target.graphNode && e.shiftKey) {
+                    let line = new LeaderLine(this.focalNode, e.target.graphNode)
+                    this.focalNode.edges.push(line)
+                    e.target.graphNode.edges.push(line)
+                } else if (e.target.graphNode) {
+                    this.focalNode = e.target.graphNode
                 }
             }
         )
